fed94ce1d47268b4b8e06e172c27a40a4a489db1
yfzhou <zhouyifan07@corp.netease.com>
2025-03-13 00:52:15 +0800

    add go client

---
+++ client-go/client.go
@@ -0,0 +1,50 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+// TextEmbeddingRequest represents the request payload for text embedding.
+type TextEmbeddingRequest struct {
+	Text string `json:"text"`
+}
+
+// TextEmbeddingResponse represents the response from the server.
+type TextEmbeddingResponse struct {
+	Embedding []float64 `json:"embedding"`
+}
+
+// GetTextEmbedding sends a text to the server and returns the embedding.
+func GetTextEmbedding(serverURL, text string) ([]float64, error) {
+	requestBody, err := json.Marshal(TextEmbeddingRequest{Text: text})
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(serverURL+"/embedding/text", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("server returned non-200 status code")
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response TextEmbeddingResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Embedding, nil
+}
---
+++ client-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/yfzhou0904/embedding-service/go-client
+
+go 1.23.4
